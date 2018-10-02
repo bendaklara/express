314bb44dc325bb379ac7be00b518f99112e8ee5f
Benda Klara <perfectyello@gmail.com>
2018-10-02 14:01:51 +0200

    Deploy

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
 var express = require('express');
 var app=express();
+app.set('view engine', 'ejs');
 
-app.get('/privacy',function(req,res)
-{
-	res.send('<h1>Privacy policy</h1>');	
-}
-);
+//app.use(express.static(__dirname + '/public'));
 
-app.get('/*',function(req,res)
-{
-	res.send('<h1>Welcome</h1>');	
-}
-);
+app.get('/privacypolicy/privacypolicy.html', function(req, res) {
+	var botname="Translate-a-bot Facebook app"
+    res.render('pages/privacy',{botname:botname});
+});
+app.get('/privacypolicy/placelookup_policy.html', function(req, res) {
+	var botname="Place Lookup Messenger bot"
+    res.render('pages/privacy',{botname:botname});
+});
+app.get('/privacypolicy/privacypolicybirdsbot.html', function(req, res) {
+	var botname="Birds of a Feather Messenger bot"
+    res.render('pages/privacy',{botname:botname});
+});
+app.get('/privacypolicy/myfbceleb_adatnyilatkozat.html', function(req, res) {
+	var botname="My FB Celeb bot"
+    res.render('pages/privacy_hun',{botname:botname});
+});
+app.get('/privacypolicy/adatnyilatkozat.html', function(req, res) {
+	var botname="Ötöslottó bot"
+    res.render('pages/privacy_hun',{botname:botname});
+});
 
-
-console.log('Server started on port 3000');
-app.listen(3000);
+console.log('server started on localhost:80');
+app.listen(80);
